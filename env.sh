a0d12cb9710911ee9517e4458e8b31f32b07ff6e
silvzr <117378129+silvzr@users.noreply.github.com>
2024-09-29 19:50:34 +0200

    Adapt to ubuntu-latest changes
    ubuntu-latest is now 24.04 in which python2 is deprecated. In this way it can still be installed when a different OS version is choosen.

--- env.sh
+++ env.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+UBUNTU_VERSION=$(lsb_release -sr)
+
 # Remove FireFox
 apt remove firefox
 apt autoremove
@@ -7,5 +9,7 @@ apt autoremove
 # Environment Setup
 apt-get update && apt-get upgrade -y
 
-# Install python2 (required for old mtkdtboimg script)
-apt install python2
+# Install python2 if Ubuntu version is lower than 24.04
+if [[ $UBUNTU_VERSION != "24.04" ]]; then
+    apt install python2
+fi
